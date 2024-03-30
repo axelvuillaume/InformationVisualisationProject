f7194db1cafa166a59621cd5d223aca17377db88
LaurensDevloo <laurens.devloo@hotmail.com>
2024-03-30 16:30:59 +0100

    Adding gauche in different python file

---
+++ components/gauche.py
@@ -0,0 +1,36 @@
+import plotly.graph_objects as go
+
+value = 75
+remaingin = 100
+
+def decide_colour(value):
+    if value < 50:
+        return "Red"
+    elif (value >= 50) and (value < 60):
+        return "Orange"
+    else:
+        return "Green"
+    
+x=  [0, 1]
+y = [0, 1]
+
+d = {'x': x, 'y': y}
+gauge = {'axis': {'range': [None, 100], 'tickwidth': 1, 'tickcolor': "darkblue"},
+         'bar': {'color': decide_colour(value)}, 'bgcolor': "white",
+         'borderwidth': 2,
+         'bordercolor': "gray",
+         'steps': [{'range': [0, value], 'color': 'lightblue'}, {'range': [value, 100], 'color': 'lightgray'}],
+         'shape': "angular"}
+number = {'font': {'size': 100, 'color': decide_colour(value)}, 'suffix': "%"}
+
+fig = go.Figure(go.Indicator(
+    mode="gauge+number",
+    value=value,
+    domain= d,
+    gauge= gauge,
+    number=number
+))
+
+fig.update_layout(height=400)
+
+fig.show()

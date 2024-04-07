87ac70aa0f7b4536d3004afd40fd3556322d10f1
LaurensDevloo <laurens.devloo@hotmail.com>
2024-04-07 12:18:22 +0200

    To get different datasets out

--- components/data_access.py
+++ components/data_access.py
@@ -23,6 +23,24 @@ def get_data_specific(specific):
 
     return output
 
+# get_data_apart_sub(datasets)
+#   get_data will read the needed data out of the CSV-files and put these in a pandas dataframe.
+#
+#   params:     datasets:               Array of dataset names.
+#   returns:    categories_data:        DataFrame
+#               cleaned_data:           DataFrame
+#               full_audio_data:        DataFrame
+#               genres_data:            DataFrame
+#               supported_audio_data:   DataFrame
+def get_data_apart_sub(datasets):
+    output =  []
+
+    for dataset in datasets:
+        tmp = get_data_specific(dataset)
+
+        output.append(tmp)
+    
+    return output
 # get_data_apart()
 #   get_data will read the needed data out of the CSV-files and put these in a pandas dataframe.
 #
@@ -42,6 +60,16 @@ def get_data_apart():
     supported_audio_data  = get_data_specific(all_datasets[4])
 
     return categories_data, cleaned_data, full_audio_data, genres_data, supported_audio_data
+# get_data_together_sub(datasets)
+#   get_data_sub will read given data.
+#
+#   params:     datasets:   Array of datasets.
+#   returns:    output:     DataFrame
+def get_data_together_sub(datasets):
+    first 
+    output = pd.concat(tobe_merged, axis=0)
+
+    return output
 # get_data_together()
 #   get_data will read the needed data out of the CSV-files and put these in a pandas dataframe.
 #
@@ -54,3 +82,5 @@ def get_data_together():
     output = pd.concat(tobe_merged, axis=0)
 
     return output
+
+get_data_apart_sub()

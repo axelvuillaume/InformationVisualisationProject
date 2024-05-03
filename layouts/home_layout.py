ccd738d13322cba2d87c42c9453f548873999ea3
axelvuillaume <axelvuillaume@hotmail.fr>
2024-05-03 18:46:14 +0200

    Change css

--- layouts/home_layout.py
+++ layouts/home_layout.py
@@ -16,6 +16,7 @@ from utils.data_processing import get_n_best_gen_or_cat_by_hours, get_game_list_
 
 def generate_home_layout():
     return html.Div(
+        className ="wrapper",
         id='main-canva',
         children=[
             
@@ -23,79 +24,24 @@ def generate_home_layout():
             html.H1('STEAM DASHBOARD'),
                 dcc.Link('Home', href='/home', id='home-link', className='button'),
                 dcc.Link('Profile', href='/profile', id='profile-link', className='button')
-        ], className='button-container',style={'width': '100%'} ),
+        ], className='button-container one'),
 
-            dcc.Store(id='steam-id-store'), 
+            
             # once update-user-data is finished, store the steam id, once this changes the graph updates are triggered
             # see example 1 for more info : https://dash.plotly.com/sharing-data-between-callbacks
 
-            html.Div([
-                "Steam Id: ",
-                dcc.Input(id='steam-id', placeholder="Insert Steam Id", value='76561198150561997'),
-                html.Button(id='submit-steamid', n_clicks=0, children='Submit')
-            ], style={'color': 'white'}),
 
+
+        
             html.Div(
-                className="component-container",
+                className="component-container two",
                 children=[
                     generate_top_games_chart(cleaned_games, n=10)
                 ]
             ),
+
             html.Div(
-                className="component-container",
-                id='user_vs_friends_panel',
-                children=[
-                    generate_user_vs_friends_panel()
-                ]
-            ),
-            html.Div(
-                className="component-container",
-                id = 'bubble-chart',
-                children=[
-                    #bubble(n=8)
-                ]
-            ),
-            html.Div(
-                className="component-container",
-                id = 'user-playtime-chart',
-                children=[
-                    html.Div(
-                        id = 'user-playtime-chart',
-                    ),
-                    html.Div(
-                        style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'flex-start', 'align-items': 'center'},
-                        children=[
-                            html.H4("Game Amount: ", style={'color': 'white'}),
-                            html.Div(
-                                id = 'user-game-slider',
-                                style={'width': '80%', 'margin-top': '1em'}
-                            )
-                        ]
-                    ),
-                    # TODO: check if we need the genre slider else remove
-                    html.Div(
-                        style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'flex-start', 'align-items': 'center'},
-                        children=[
-                            html.H4("Genre Amount: ", style={'color': 'white'}),
-                            html.Div(
-                                id = 'genre-slider',
-                                style={'width': '80%', 'margin-top': '1em'},
-                                children=[
-                                    genre_slider()
-                                ]
-                            )
-                        ]
-                    ),
-               ],
-            ),
-            html.Div(
-                className="component-container",
-                id = 'detail-playtime-chart',
-                children=[
-                ]
-            ),
-            html.Div(
-                className="component-container",
+                className="component-container three",
                 id = 'gauges',
                 children=[
                     html.Div(
@@ -118,14 +64,14 @@ def generate_home_layout():
                 ]
             ),
             html.Div(
-                className="component-container",
+                className="component-container four",
                 id='sunburst_by_categries',
                 children=[
                     graph_sunburst()
             ]
             ),
             html.Div(
-                className="component-container",
+                className="component-container five",
                 id='map_by_supported_languages',
                 children=[
                     graph_map()
@@ -143,46 +89,3 @@ def update_links(pathname):
     home_class = 'button active' if pathname == '/home' else 'button'
     profile_class = 'button active' if pathname == '/profile' else 'button'
     return home_class, profile_class
-
-
-@callback(
-    Output('steam-id-store', 'data'),
-    Input("submit-steamid", "n_clicks"),
-    State("steam-id", "value"),
-    running=[(Output("submit-steamid", "disabled"), True, False)] 
-    #While callback is running -> button property disabled = True; when done, disabled = False
-)
-def update_current_user(_, steamid):
-    current_user.steamid = steamid
-    return steamid
-
-@callback(
-    Output('bubble-chart', "children"),
-    Input('steam-id-store', 'data')
-)
-def bubble(_):
-    return bubble_chart()
-
-@callback(
-    Output('user-playtime-chart', "children"),
-    Input('steam-id-store', 'data'),
-    Input("user-playtime-slider", "value"),
-    Input("genre-slider", "value")
-)
-def playtime_chart(_, games_slider_value, genre_slider_value):
-    return playtime_per_genre(games_amount=games_slider_value, genres_amount=genre_slider_value)
-
-@callback(
-    Output('user-game-slider', "children"),
-    Input('steam-id-store', 'data')
-)
-def playtime_slider(_):
-    return steam_game_slider()
-
-@callback(
-    Output('detail-playtime-chart', 'children'),
-    Input('playtime-bar-chart-figure', 'clickData'))
-def display_click_data(clickData):
-    # 'x' key of the clickData contains the genre name
-    genre_name = clickData['points'][0]['x'] if clickData else None
-    return playtime_games_per_genre(genre_name)
--- layouts/profile_layout.py
+++ layouts/profile_layout.py
@@ -23,6 +23,116 @@ def generate_profile_layout():
                 dcc.Link('Home', href='/home', id='home-link', className='button'),
                 dcc.Link('Profile', href='/profile', id='profile-link', className='button')
         ], className='button-container',style={'width': '100%'} ),
+            
+        
+        dcc.Store(id='steam-id-store'), 
+        
+        
+            html.Div([
+                "Steam Id: ",
+                dcc.Input(id='steam-id', placeholder="Insert Steam Id", value='76561198150561997'),
+                html.Button(id='submit-steamid', n_clicks=0, children='Submit')
+            ], style={'color': 'white'}),
+            
+                        html.Div(
+                className="component-container",
+                id='user_vs_friends_panel',
+                children=[
+                    generate_user_vs_friends_panel()
+                ]
+            ),
+                        
+                    
+            html.Div(
+                className="component-container",
+                id = 'bubble-chart',
+                children=[
+                    #bubble(n=8)
+                ]
+            ),
+            
+            
+            html.Div(
+                className="component-container",
+                id = 'user-playtime-chart',
+                children=[
+                    html.Div(
+                        id = 'user-playtime-chart',
+                    ),
+                    html.Div(
+                        style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'flex-start', 'align-items': 'center'},
+                        children=[
+                            html.H4("Game Amount: ", style={'color': 'white'}),
+                            html.Div(
+                                id = 'user-game-slider',
+                                style={'width': '80%', 'margin-top': '1em'}
+                            )
+                        ]
+                    ),
+                    # TODO: check if we need the genre slider else remove
+                    html.Div(
+                        style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'flex-start', 'align-items': 'center'},
+                        children=[
+                            html.H4("Genre Amount: ", style={'color': 'white'}),
+                            html.Div(
+                                id = 'genre-slider',
+                                style={'width': '80%', 'margin-top': '1em'},
+                                children=[
+                                    genre_slider()
+                                ]
+                            )
+                        ]
+                    ),
+               ],
+            ),
+            html.Div(
+                className="component-container",
+                id = 'detail-playtime-chart',
+                children=[
+                ]
+            ),
         ]
     )
     
+
+@callback(
+    Output('steam-id-store', 'data'),
+    Input("submit-steamid", "n_clicks"),
+    State("steam-id", "value"),
+    running=[(Output("submit-steamid", "disabled"), True, False)] 
+    #While callback is running -> button property disabled = True; when done, disabled = False
+)
+def update_current_user(_, steamid):
+    current_user.steamid = steamid
+    return steamid
+
+@callback(
+    Output('bubble-chart', "children"),
+    Input('steam-id-store', 'data')
+)
+def bubble(_):
+    return bubble_chart()
+
+@callback(
+    Output('user-playtime-chart', "children"),
+    Input('steam-id-store', 'data'),
+    Input("user-playtime-slider", "value"),
+    Input("genre-slider", "value")
+)
+def playtime_chart(_, games_slider_value, genre_slider_value):
+    return playtime_per_genre(games_amount=games_slider_value, genres_amount=genre_slider_value)
+
+@callback(
+    Output('user-game-slider', "children"),
+    Input('steam-id-store', 'data')
+)
+def playtime_slider(_):
+    return steam_game_slider()
+
+@callback(
+    Output('detail-playtime-chart', 'children'),
+    Input('playtime-bar-chart-figure', 'clickData'))
+def display_click_data(clickData):
+    # 'x' key of the clickData contains the genre name
+    genre_name = clickData['points'][0]['x'] if clickData else None
+    return playtime_games_per_genre(genre_name)
